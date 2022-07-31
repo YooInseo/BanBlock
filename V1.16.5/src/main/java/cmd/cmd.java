698cc51aac8017e2d9f4d445a80e3ac85b7bf287
BuildTools <unconfigured@null.spigotmc.org>
2022-08-01 08:20:02 +0900

    V1.16.5

---
+++ V1.16.5/src/main/java/cmd/cmd.java
@@ -0,0 +1,58 @@
+package cmd;
+
+import Data.Data;
+import main.Main;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+public class cmd implements CommandExecutor {
+
+    @Override
+    public boolean onCommand( CommandSender sender,  Command command,  String label,String[] args) {
+        Player player = (Player) sender;
+
+        if (args.length == 0) {
+            player.sendMessage("§7/banblock <worldname>");
+            player.sendMessage("§7/밴블록 <worldname>");
+
+        } else{
+            if(args[0].equalsIgnoreCase("reload")) {
+                if (player.isOp()) {
+                    Main.plugin.config.reloadConfig();
+                    player.sendMessage("§a성공적으로 리로드를 완료 했습니다!");
+                }
+
+            } else{
+                if (args.length == 1) {
+                    String name = args[0];
+                    if(name.equalsIgnoreCase("all")){
+                        Main.plugin.data.put(player.getUniqueId(), new Data(player));
+                        Data data = Main.plugin.data.get(player.getUniqueId());
+                        data.setPlayer(player);
+                        data.banblock();
+                        data.setWorld(name);
+
+                        data.OpenGUI(name);
+                        data.getPageclass().nextsign();
+                    } else if (Main.plugin.config.getConfig().getList("worlds").contains(name)) {
+                        Main.plugin.data.put(player.getUniqueId(), new Data(player));
+                        Data data = Main.plugin.data.get(player.getUniqueId());
+                        data.setPlayer(player);
+                        data.banblock();
+                        data.setWorld(name);
+
+                        data.OpenGUI(name);
+                        data.getPageclass().nextsign();
+
+                    }
+                }
+            }
+
+        }
+
+        return false;
+    }
+}
---
+++ V1.16.5/src/main/java/events/CloseEvent.java
@@ -0,0 +1,16 @@
+package events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
+
+public class CloseEvent implements Listener {
+    @EventHandler
+    public void onClose(InventoryCloseEvent event) {
+        Inventory inv = event.getInventory();
+        Player player = (Player) event.getPlayer();
+
+    }
+}
